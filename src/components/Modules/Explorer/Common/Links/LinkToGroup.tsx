641f54c154c8583e7a9f90d0666c954be4612f11
AD <65587162+algodesk-io@users.noreply.github.com>
2022-08-17 17:20:22 +0530

    group txn icon

--- src/components/Modules/Explorer/Common/Links/LinkToGroup.tsx
+++ src/components/Modules/Explorer/Common/Links/LinkToGroup.tsx
@@ -1,12 +1,12 @@
 import {IconButton, Link, Tooltip} from "@mui/material";
 import React from "react";
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import FilePresentIcon from '@mui/icons-material/FilePresent';
 
 function LinkToGroup({id, blockId, icon = false}): JSX.Element {
 
     return <Link href={"/explorer/group/" + encodeURIComponent(id) + '/' + blockId}>{icon ? <Tooltip title="This is one of the group transaction. Click to view all the groups transactions.">
         <IconButton size={"small"}>
-            <AttachFileIcon style={{fontSize: 16, color: '#000'}}></AttachFileIcon>
+            <FilePresentIcon style={{fontSize: 16, color: '#000'}}></FilePresentIcon>
         </IconButton>
     </Tooltip> : id}</Link>;
 }
