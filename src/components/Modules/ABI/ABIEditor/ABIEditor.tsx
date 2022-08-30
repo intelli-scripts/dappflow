79dd37e3e57300da42a17d28dbbea15a47f13115
AD <65587162+algodesk-io@users.noreply.github.com>
2022-08-30 13:58:01 +0530

    networks header

--- src/components/Modules/ABI/ABIEditor/ABIEditor.tsx
+++ src/components/Modules/ABI/ABIEditor/ABIEditor.tsx
@@ -5,10 +5,12 @@ import {shadedClr, shadedClr2} from "../../../../utils/common";
 import ABIMethod from "../ABIMethod/ABIMethod";
 import JsonViewer from "../../../Common/JsonViewer/JsonViewer";
 import {ABI} from "../../../../packages/abi/classes/ABI";
-import {Box, Chip} from "@mui/material";
+import {Box, Chip, Grid} from "@mui/material";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../../redux/store";
 import {CoreVersionsCheck} from "../../../../packages/core-sdk/classes/CoreVersionsCheck";
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import IntegrationInstructionsOutlinedIcon from '@mui/icons-material/IntegrationInstructionsOutlined';
 
 function ABIEditor(props): JSX.Element {
     let abi: A_ABI = props.abi;
@@ -46,27 +48,47 @@ function ABIEditor(props): JSX.Element {
                     <div className="abi-body">
                         <Box className="networks" sx={{borderColor: shadedClr2 + ' !important'}}>
                             <div className="networks-header">
+                                <PodcastsIcon color={"primary"}></PodcastsIcon>
                                 Networks
                             </div>
                             <div className="networks-body">
                                 {!hasNetworks ? <Box sx={{ color: 'warning.main', fontSize: 14}}>
                                     No networks are configured
                                 </Box> : <div>
+                                    <div className="network-table-header">
+                                        <Grid container spacing={0}>
+                                            <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+                                                Genesis hash b64
+                                            </Grid>
+                                            <Grid item xs={12} sm={6} md={8} lg={8} xl={8}>
+                                                Application ID
+                                            </Grid>
+                                        </Grid>
+                                    </div>
+
                                     {Object.keys(networks).map((name) => {
                                         return <div className="network">
-                                            <div className="network-name">
-                                                <Box sx={{color: 'primary.main'}}>
-                                                    {name}
-                                                </Box>
-                                            </div>
-                                            <div className="app-id">
-                                                {versionsCheckInstance.getGenesisHashB64() === name ? <div>
-                                                    <span>{networks[name].appID}</span>
-                                                    <Chip label="Open App" className="app-link" variant={"outlined"} size={"small"} color={"primary"} onClick={() => {
-                                                        window.open("/explorer/application/" + networks[name].appID , "_blank");
-                                                    }}></Chip>
-                                                </div> : networks[name].appID}
-                                            </div>
+                                            <Grid container spacing={0}>
+                                                <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+                                                    <div className="network-name">
+                                                        <Box sx={{color: 'primary.main'}}>
+                                                            {name}
+                                                        </Box>
+                                                    </div>
+                                                </Grid>
+                                                <Grid item xs={12} sm={6} md={8} lg={8} xl={8}>
+                                                    <div className="app-id">
+                                                        {versionsCheckInstance.getGenesisHashB64() === name ? <div>
+                                                            <span>{networks[name].appID}</span>
+                                                            <Chip label="Open App" className="app-link" variant={"outlined"} size={"small"} color={"primary"} onClick={() => {
+                                                                window.open("/explorer/application/" + networks[name].appID , "_blank");
+                                                            }}></Chip>
+                                                        </div> : networks[name].appID}
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+
+
                                         </div>;
                                     })}
                                 </div>}
@@ -75,6 +97,7 @@ function ABIEditor(props): JSX.Element {
 
                         <div className="methods">
                             <div className="methods-header">
+                                <IntegrationInstructionsOutlinedIcon color={"primary"}></IntegrationInstructionsOutlinedIcon>
                                 Methods
                             </div>
                             <div className="methods-body">
