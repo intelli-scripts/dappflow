38a153c01f7148c0a470c63b069f3425695b58b9
shiva prasad manupadi <shivaprasadmanupadi@shivas-MacBook-Pro.local>
2022-10-25 20:28:57 +0530

    abi executor base component

---
+++ src/components/Modules/ABI/ABIMethodExecutor/ABIMethodExecutor.tsx
@@ -0,0 +1,13 @@
+import './ABIMethodExecutor.scss';
+import React from "react";
+
+function ABIMethodExecutor(): JSX.Element {
+    return (<div className={"abi-method-executor-wrapper"}>
+        <div className={"abi-method-executor-container"}>
+
+
+        </div>
+    </div>);
+}
+
+export default ABIMethodExecutor;
