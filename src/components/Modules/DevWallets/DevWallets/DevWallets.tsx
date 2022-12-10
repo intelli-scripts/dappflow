78c02c25f930baeb0ee6dc58d52cc52c2165b3c3
shiva prasad manupadi <shivaprasadmanupadi@shivas-MacBook-Pro.local>
2022-12-10 23:37:18 +0530

    dev wallet actions

--- src/components/Modules/DevWallets/DevWallets/DevWallets.tsx
+++ src/components/Modules/DevWallets/DevWallets/DevWallets.tsx
@@ -3,11 +3,12 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import AddIcon from "@mui/icons-material/Add";
 import {Button} from "@mui/material";
-import {shadedClr} from "../../../../utils/common";
+import {copyContent, shadedClr} from "../../../../utils/common";
 import {createDevWallet, deleteDevWallet, loadDevWallets} from "../../../../redux/devWallets/actions/devWallets";
 import {generateAccount, secretKeyToMnemonic} from 'algosdk'
 import {A_Dev_Wallet} from "../../../../packages/dev-wallets/types";
 import {RootState} from "../../../../redux/store";
+import {showSnack} from "../../../../redux/common/actions/snackbar";
 
 
 function DevWallets(): JSX.Element {
@@ -15,6 +16,7 @@ function DevWallets(): JSX.Element {
 
     const dispatch = useDispatch();
     const devWallets = useSelector((state: RootState) => state.devWallets);
+    const wallet = useSelector((state: RootState) => state.wallet);
     const {wallets} = devWallets;
 
 
@@ -55,18 +57,42 @@ function DevWallets(): JSX.Element {
 
                 <div className={"dev-wallets-list-wrapper"}>
                     <div className={"dev-wallets-list-container"} style={{background: shadedClr}}>
-                        {wallets.map((wallet) => {
-                            return <div className="wallet-wrapper" key={wallet.address}>
+                        {wallets.map((devWallet) => {
+                            return <div className="wallet-wrapper" key={devWallet.address}>
                                 <div className="wallet-container">
                                     <div className="wallet-address">
-                                        {wallet.address}
+                                        {devWallet.address}
+                                        <div style={{marginTop: '10px'}}>
+                                            <Button color={"primary"}
+                                                    size={"small"}
+                                                    variant={"text"}
+                                                    onClick={() => {
+                                                        window.open('/explorer/account/' + devWallet.address, '_blank');
+                                                    }}
+                                            >View in explorer</Button>
+                                            <Button color={"primary"}
+                                                    size={"small"}
+                                                    variant={"text"}
+                                                    onClick={(event) => {
+                                                        copyContent(event, dispatch, devWallet.address, 'Address copied');
+                                                    }}
+                                            >Copy</Button>
+                                        </div>
                                     </div>
                                     <div className="wallet-actions">
                                         <Button color={"warning"}
                                                 size={"small"}
                                                 variant={"outlined"}
                                                 onClick={() => {
-                                                    dispatch(deleteDevWallet(wallet.address));
+                                                    if (wallet.account.address === devWallet.address) {
+                                                        dispatch(showSnack({
+                                                            severity: 'error',
+                                                            message: 'You are trying to delete the connected wallet. please disconnect you wallet before deleting.'
+                                                        }));
+                                                        return;
+                                                    }
+
+                                                    dispatch(deleteDevWallet(devWallet.address));
                                                 }}
                                         >Delete</Button>
                                     </div>
